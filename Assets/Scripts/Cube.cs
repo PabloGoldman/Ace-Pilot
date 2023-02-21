16421aed98758269f23d3d7cf845b8f30bca5b43
PabloGoldman <pablo.goldmanarca@gmail.com>
2023-02-20 22:51:52 -0300

    Added a lot of packages

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -5,14 +5,14 @@ public class Cube : MonoBehaviour
 {
     private int bulletsNeeded = 3; // The number of bullets needed to destroy the cube
 
-    private TextMeshProUGUI numberTextMesh; // The TextMesh component used to display the number
+    //private TextMeshProUGUI numberTextMesh; // The TextMesh component used to display the number
 
     void Start()
     {
         // Get the TextMesh component from the cube's child object
-        numberTextMesh = GetComponentInChildren<TextMeshProUGUI>();
+        //numberTextMesh = GetComponentInChildren<TextMeshProUGUI>();
         // Set the text to display the number of bullets needed
-        numberTextMesh.text = bulletsNeeded.ToString();
+        //numberTextMesh.text = bulletsNeeded.ToString();
     }
 
     public void SetBulletRequirement(int setBulletNumber)
