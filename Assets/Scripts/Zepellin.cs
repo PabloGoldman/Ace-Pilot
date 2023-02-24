faaae219b1a9b4cb28beda0a5f7d35928d84cf12
PabloGoldman <pablo.goldmanarca@gmail.com>
2023-02-23 22:22:24 -0300

    Added a lot of models

--- Assets/Scripts/Zepellin.cs
+++ Assets/Scripts/Zepellin.cs
@@ -1,7 +1,46 @@
+using System.Collections;
 using UnityEngine;
 
 public class Zepellin : MonoBehaviour
 {
+    Rigidbody rb;
+
+    float force = 500;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(AddForceCoroutine());
+    }
+
+    void AddRandomForce()
+    {
+        int random = Random.Range(1, 3);
+
+        if (random == 1)
+        {
+            rb.AddForce(Vector3.right * force, ForceMode.Force);
+        }
+        else
+        {
+            rb.AddForce(Vector3.right * -force, ForceMode.Force);
+
+        }
+    }
+
+    IEnumerator AddForceCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(2f);
+            AddRandomForce();
+        }
+    }
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Bullet"))
