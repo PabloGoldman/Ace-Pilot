3c69014acbdc35b02cca26f8b990c5933fc73e0d
PabloGoldman <pablo.goldmanarca@gmail.com>
2023-02-23 23:14:35 -0300

    Updated chunk generation

--- Assets/Scripts/TeleportParentOnPlayerTrigger.cs
+++ Assets/Scripts/TeleportParentOnPlayerTrigger.cs
@@ -5,14 +5,13 @@ public class TeleportParentOnPlayerTrigger : MonoBehaviour
     // The position to teleport the parent object to
     Vector3 teleportPosition;
 
-    float offsetZ = 372;
+    float offsetZ = 429f;  //Cantidad de building multiplicado por 78.2
 
     private void OnTriggerEnter(Collider other)
     {
         // Check if the other object is the player
         if (other.CompareTag("Player"))
         {
-            Debug.Log("asd");
             teleportPosition = new Vector3(0, 0, transform.position.z + offsetZ);
             // Teleport the parent object to the given position
             transform.parent.position = teleportPosition;
