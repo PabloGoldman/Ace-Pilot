e90b1616b3904130e8c649fe0f4a1378029e78d4
PabloGoldman <pablo.goldmanarca@gmail.com>
2023-02-24 00:37:30 -0300

    Improved car behaviour

--- Assets/Scripts/CarBehaviour.cs
+++ Assets/Scripts/CarBehaviour.cs
@@ -6,9 +6,18 @@ public class CarBehaviour : MonoBehaviour
 {
     float speed;  // The speed at which the object moves forward
 
+    Vector3 initialPos;
+
     private void Start()
     {
         speed = Random.Range(1, 10);
+
+        initialPos = transform.localPosition;
+    }
+
+    public void SetToInitialPos()
+    {
+        transform.localPosition = initialPos;
     }
 
     // Update is called once per frame
--- Assets/Scripts/TeleportParentOnPlayerTrigger.cs
+++ Assets/Scripts/TeleportParentOnPlayerTrigger.cs
@@ -5,28 +5,9 @@ public class TeleportParentOnPlayerTrigger : MonoBehaviour
     // The position to teleport the parent object to
     Vector3 teleportPosition;
 
-    public GameObject[] car;
-
-    public Transform[] carInitialPos;
+    public CarBehaviour[] carList;
 
     float offsetZ = 429f;
-
-    private void Start()
-    {
-        for (int i = 0; i < car.Length; i++)
-        {
-            carInitialPos[i] = car[i].transform;
-        }
-    }
-
-    void SetNewCarPos()
-    {
-        for (int i = 0; i < car.Length; i++)
-        {
-            car[i].transform.position = new Vector3(carInitialPos[i].position.x, carInitialPos[i].position.y, carInitialPos[i].position.z - 50f);
-            car[i].transform.rotation = carInitialPos[i].rotation;
-        }
-    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -36,7 +17,11 @@ public class TeleportParentOnPlayerTrigger : MonoBehaviour
             teleportPosition = new Vector3(0, 0, transform.position.z + offsetZ);
             // Teleport the parent object to the given position
             transform.parent.position = teleportPosition;
-            SetNewCarPos();
+
+            foreach (CarBehaviour car in carList)
+            {
+                car.SetToInitialPos();
+            }
         }
     }
 }
