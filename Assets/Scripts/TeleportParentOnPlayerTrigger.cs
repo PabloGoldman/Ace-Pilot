f8114c62daf7b1c76e918e27fc67f20fc7552d62
PabloGoldman <pablo.goldmanarca@gmail.com>
2023-02-27 20:36:50 -0300

    Added new street fbx

--- Assets/Scripts/TeleportParentOnPlayerTrigger.cs
+++ Assets/Scripts/TeleportParentOnPlayerTrigger.cs
@@ -7,14 +7,14 @@ public class TeleportParentOnPlayerTrigger : MonoBehaviour
 
     public CarBehaviour[] carList;
 
-    float offsetZ = 351f;  //61.285 multiplicado la cantidad de chunks
+    float offsetZ = 353f;  //61.285 multiplicado la cantidad de chunks
 
     private void OnTriggerEnter(Collider other)
     {
         // Check if the other object is the player
         if (other.CompareTag("Player"))
         {
-            teleportPosition = new Vector3(0, 0, transform.position.z + offsetZ);
+            teleportPosition = new Vector3(0.144f, 0.188f, transform.position.z + offsetZ);
             // Teleport the parent object to the given position
             transform.parent.position = teleportPosition;
 
