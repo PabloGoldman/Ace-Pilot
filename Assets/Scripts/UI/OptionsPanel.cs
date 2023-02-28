0ec95f95d9cb2ec68bf3ab0276d05b1498c40ee6
PabloGoldman <pablo.goldmanarca@gmail.com>
2023-02-27 21:24:31 -0300

    Credits button now appears after losing

--- Assets/Scripts/UI/OptionsPanel.cs
+++ Assets/Scripts/UI/OptionsPanel.cs
@@ -4,40 +4,40 @@ using UnityEngine;
 
 public class OptionsPanel : MonoBehaviour
 {
-    public GameObject settingsPanel;
+    //public GameObject settingsPanel;
 
-    Animator settingsPanelAnimator;
+    //Animator settingsPanelAnimator;
 
-    bool inSettings = false;
+    //bool inSettings = false;
 
-    bool firstTrigger = true;
+    //bool firstTrigger = true;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        settingsPanelAnimator = settingsPanel.GetComponent<Animator>();
-    }
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    settingsPanelAnimator = settingsPanel.GetComponent<Animator>();
+    //}
 
-    public void TriggerSettingsPanel()
-    {
-        if (!firstTrigger)
-        {
-            if (inSettings)
-            {
-                settingsPanelAnimator.SetTrigger("Close");
-                inSettings = false;
-            }
-            else
-            {
-                settingsPanelAnimator.SetTrigger("Open");
-                inSettings = true;
-            }
-        }
-        else
-        {
-            settingsPanel.SetActive(true);
-            inSettings = true;
-            firstTrigger = false;
-        }
-    }
+    //public void TriggerSettingsPanel()
+    //{
+    //    if (!firstTrigger)
+    //    {
+    //        if (inSettings)
+    //        {
+    //            settingsPanelAnimator.SetTrigger("Close");
+    //            inSettings = false;
+    //        }
+    //        else
+    //        {
+    //            settingsPanelAnimator.SetTrigger("Open");
+    //            inSettings = true;
+    //        }
+    //    }
+    //    else
+    //    {
+    //        settingsPanel.SetActive(true);
+    //        inSettings = true;
+    //        firstTrigger = false;
+    //    }
+    //}
 }
